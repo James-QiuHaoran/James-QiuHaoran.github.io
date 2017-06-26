c642817e4c52707700c73254053926e41b3ae3e6
James-QiuHaoran <jamesqiu@hku.hk>
2017-06-26 13:33:21 +0800

    Modify the send email part.

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "example@example.net";
+$address = "jamesqiu@hku.hk";
 
 
 // Configuration option.
